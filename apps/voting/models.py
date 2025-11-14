fe3b608e6599ed61fe33f957089892d775159369
DanuloM <danulo1337@gmail.com>
2025-11-14 14:55:38 +0200

    add models and migrate

--- apps/voting/models.py
+++ apps/voting/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from apps.menus.models import Menu
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Vote(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="votes")
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="votes")
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["employee", "date"]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        username = getattr(self.employee, "username", None)
+        return f"{username} voted for {self.menu}"
