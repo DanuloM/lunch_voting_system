e4f4c8b48c1a25b9acc1de1f0a00f76d17bfa330
Danulo <myska.danylo.ki.2023@lpnu.ua>
2025-11-14 20:54:25 +0200

    Implement restaurants endpoints

--- apps/restaurants/views.py
+++ apps/restaurants/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 
-# Create your views here.
+
+class RestaurantListCreateView(generics.ListCreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RestaurantRetrieveView(generics.RetrieveAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticated]
